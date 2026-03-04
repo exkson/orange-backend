44ac9b4f0e375aef4788888c5efd2936ee3c80cb
Parfait Detchenou <pdetchenou@gmail.com>
2026-03-04 18:02:10 +0100

    Implement device authentication and load credentials from file

--- api.py
+++ api.py
@@ -1,19 +1,39 @@
-from datetime import datetime
+import contextlib
 from pathlib import Path
 
 from starlette.applications import Starlette
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 
-app = Starlette(debug=True)
-
 chunk_size = 1024 * 1024
+
+
+@contextlib.asynccontextmanager
+async def lifespan(app):
+    Devices.loads()
+    yield
+
+
+class Devices:
+    _devices = {}
+
+    @classmethod
+    def loads(cls):
+        with open(Path(__file__).parent.joinpath("credentials")) as f:
+            for line in f:
+                token, customer_name = line.strip().split(",")
+                cls._devices[token] = customer_name
+
+
+app = Starlette(lifespan=lifespan)
 
 
 @app.route("/logs", methods=["POST"])
 async def logs(request: Request):
-    received_at = datetime.now().isoformat().replace("T", "").rstrip("Z")
-    location = Path(__file__).parent.joinpath("static", "logs", f"{received_at}.log")
+    token = request.headers.get("X-Device-Token")
+    if token not in Devices._devices:
+        return JSONResponse({"error": "Unauthorized"}, status_code=401)
+    location = Path(__file__).parent.joinpath("static", "logs", f"{token}.log")
     async with request.form(max_files=1) as form:
         with location.open("a") as out:
             while chunk := await form["file"].read(chunk_size):
@@ -23,8 +43,10 @@ async def logs(request: Request):
 
 @app.route("/db", methods=["POST"])
 async def db(request: Request):
-    received_at = datetime.now().isoformat().replace("T", "").rstrip("Z")
-    location = Path(__file__).parent.joinpath("static", "db", f"{received_at}.db")
+    token = request.headers.get("X-Device-Token")
+    if token not in Devices._devices:
+        return JSONResponse({"error": "Unauthorized"}, status_code=401)
+    location = Path(__file__).parent.joinpath("static", "db", f"{token}.db")
 
     async with request.form(max_files=1) as form:
         with location.open("wb") as out:

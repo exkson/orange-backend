37e612874cbaeed57c5d788ca05a9ca30cd952f6
Parfait Detchenou <pdetchenou@gmail.com>
2026-03-04 21:23:04 +0100

    Add logging options to gunicorn command in start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source .venv/bin/activate
-gunicorn api:app -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
+gunicorn api:app -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT --error-logfile - --access-logfile -

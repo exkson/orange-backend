e076fcb53f662d05c0a115ba8ecfe1099f242de7
Parfait Detchenou <pdetchenou@gmail.com>
2026-04-04 19:55:00 +0100

    Update log and database file paths to include client mode

--- api.py
+++ api.py
@@ -34,12 +34,16 @@ async def logs(request: Request):
     if token not in Devices._devices:
         return JSONResponse({"error": "Unauthorized"}, status_code=401)
     id, _ = Devices._devices[token]
-    location = Path(__file__).parent.joinpath("static", "logs", f"{id}.log")
+
     async with request.form(max_files=1) as form:
+        client_mode = form.get("mode", "auto")
+        location = Path(__file__).parent.joinpath(
+            "static", client_mode, "logs", f"{id}.log"
+        )
         with location.open("a") as out:
             while chunk := await form["file"].read(chunk_size):
                 out.write(chunk.decode())
-    return JSONResponse({"saved_at": location.as_posix()})
+        return JSONResponse({"saved_at": location.as_posix()})
 
 
 @app.route("/db", methods=["POST"])
@@ -48,10 +52,13 @@ async def db(request: Request):
     if token not in Devices._devices:
         return JSONResponse({"error": "Unauthorized"}, status_code=401)
     id, _ = Devices._devices[token]
-    location = Path(__file__).parent.joinpath("static", "db", f"{id}.db")
 
     async with request.form(max_files=1) as form:
+        client_mode = form.get("mode", "auto")
+        location = Path(__file__).parent.joinpath(
+            "static", client_mode, "db", f"{id}.db"
+        )
         with location.open("wb") as out:
             while chunk := await form["file"].read(chunk_size):
                 out.write(chunk)
-    return JSONResponse({"saved_at": location.as_posix()})
+        return JSONResponse({"saved_at": location.as_posix()})
